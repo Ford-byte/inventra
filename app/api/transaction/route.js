efe619583abda4f537bb74ef1370973bd1f3710d
Clifford <c.iyac123@gmail.com>
2025-03-19 17:09:07 +0800

    update

--- app/api/transaction/route.js
+++ app/api/transaction/route.js
@@ -1,9 +1,26 @@
 import { NextResponse } from "next/server";
 import pool from "../config/route";
 import { v4 as uuidv4 } from "uuid";
+import NodeCache from "node-cache";
+
+const cache = new NodeCache({ stdTTL: 3600 });
 
 export async function GET() {
   try {
+    // Retrieve cached data
+    const cacheData = cache.get("transaction");
+
+    if (cacheData) {
+      return NextResponse.json(
+        {
+          message: "Fetch success from cache",
+          data: cacheData,
+        },
+        { status: 200 }
+      );
+    }
+
+    // If no cached data, fetch from the database
     const query = `SELECT * from user_transaction as ut LEFT JOIN transaction as t ON t.id = ut.transaction_id WHERE 1`;
 
     const results = await new Promise((resolve, reject) => {
@@ -12,6 +29,9 @@ export async function GET() {
         else resolve(results);
       });
     });
+
+    // Cache the results for future requests
+    cache.set("transaction", results);
 
     return NextResponse.json(
       {
